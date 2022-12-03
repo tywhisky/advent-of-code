94242ca76d95b46f35e7b6c3ce14461e27e729d7
Taine Fan <fty221@gmail.com>
2022-12-03 14:47:54 +0800

    Day01_PartOne

---
+++ day_01/part_one.exs
@@ -0,0 +1,13 @@
+File.read!("./day_01/input.txt")
+|> String.split("\n\n")
+|> Enum.map(fn str ->
+    str
+    |> String.split("\n")
+    |> Enum.map(fn 
+        "" -> 0
+        num -> String.to_integer(num)
+    end)
+    |> Enum.sum()
+end)
+|> Enum.max()
+|> IO.inspect()

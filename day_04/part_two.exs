aace6be804406465e8377739c0a5b8116574284f
Taine Fan <fty221@gmail.com>
2022-12-05 15:16:43 +0800

    Day04 PartTwo

---
+++ day_04/part_two.exs
@@ -0,0 +1,12 @@
+File.read!("./day_04/input.txt")
+|> String.split("\n")
+|> Enum.reject(& &1 == "")
+|> Enum.map(fn str ->
+  [first, second] = String.split(str, ",")
+  [f_1, f_2] = String.split(first, "-") |> Enum.map(&String.to_integer(&1))
+  [s_1, s_2] = String.split(second, "-") |> Enum.map(&String.to_integer(&1))
+
+  if (f_2 >= s_1 and f_1 <= s_2) or (f_1 <= s_2 and f_2 >= s_2), do: 1, else: 0
+end)
+|> Enum.sum()
+|> IO.inspect()

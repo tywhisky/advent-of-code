7e3b0abe4a812e9d8134a4fc55654fdfce2f061d
Taine Fan <fty221@gmail.com>
2022-12-09 17:45:06 +0800

    Day08 Part 2

--- day_08/part_two.scala
+++ day_08/part_two.scala
@@ -4,56 +4,74 @@ import scala.io.Source
     var input = Source.fromFile("./day_08/input.txt").getLines().
         toArray.map(line => line.split(""))
 
-    var result = input.map(line => line.map(_ => 0))
+    var result = 0
     
     for(i <- 0 to input.length - 1; j <- 0 to input(0).length - 1) {
-        // To Top
-        var t = i - 1
+        val curr = input(i)(j).toInt
+
+        var top = i - 1
         var top_count = 0
-        var top_max = "-1"
-        while(t >= 0) {
-           if(input(t)(j) > top_max) {
-                top_max = input(t)(j)
+        var top_max = -1
+        while(top >= 0) {
+            var iter = input(top)(j).toInt
+            if(iter >= curr && iter > top_max) {
+               top_count += 1 
+            }
+            if(iter < curr && iter >= top_max) {
                 top_count += 1
-           }
-            t -= 1
-        }
-        // To Right
-        var r = j + 1
-        var right_count = 0
-        var right_max = "-1"
-        while(r < input(0).length) {
-           if(input(i)(r) > right_max) {
-                right_max = input(i)(r)
-                right_count += 1
-           }
-            r += 1
-        }
-        // To Bottom
-        var b = i + 1
-        var bottom_count = 0
-        var bottom_max = "-1"
-        while(b < input.length) {
-           if(input(b)(j) > bottom_max) {
-                bottom_max = input(b)(j)
-                bottom_count += 1
-           }
-            b += 1
-        }
-        // To Left
-        var l = j - 1
-        var left_count = 0
-        var left_max = "-1"
-        while(l >= 0) {
-           if(input(i)(l) > left_max) {
-                left_max = input(i)(l)
-                left_count += 1
-           }
-            l -= 1
+            }
+            top_max = iter.max(top_max)
+            top -= 1
         }
 
-        result(i)(j) = top_count * right_count * bottom_count * left_count
+        var right = j + 1
+        var right_count = 0
+        var right_max = -1
+        while(right < input(0).length) {
+            var iter = input(i)(right).toInt
+            if(iter >= curr && iter > right_max) {
+               right_count += 1 
+            }
+            if(iter < curr && iter >= right_max) {
+                right_count += 1
+            }
+            right_max = iter.max(right_max)
+            right += 1
+        }
+
+        var bottom = i + 1
+        var bottom_count = 0
+        var bottom_max = -1
+        while(bottom < input.length) {
+            var iter = input(bottom)(j).toInt
+            if(iter >= curr && iter > bottom_max) {
+               bottom_count += 1 
+            }
+            if(iter < curr && iter >= bottom_max) {
+                bottom_count += 1
+            }
+            bottom_max = iter.max(bottom_max)
+            bottom += 1
+        }
+
+        var left = j - 1
+        var left_count = 0
+        var left_max = -1
+        while(left >= 0) {
+            var iter = input(i)(left).toInt
+            if(iter >= curr && iter > left_max) {
+               left_count += 1 
+            }
+            if(iter < curr && iter >= left_max) {
+                left_count += 1
+            }
+            left_max = iter.max(left_max)
+            left -= 1
+        }
+
+        println((top_count, right_count, bottom_count, left_count))
+        result = (top_count * right_count * bottom_count * left_count).max(result)
     }
-    
-    val max = result.map(x => x.max).max
-    println(max)
+
+
+    println(result)

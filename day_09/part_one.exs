ee92d4b80bb16c6b32f5e01b6b26972adc319039
Taine Fan <fty221@gmail.com>
2022-12-10 18:21:37 +0800

    Finish Day09 PartOne

--- day_09/part_one.exs
+++ day_09/part_one.exs
@@ -1,114 +1,37 @@
 defmodule Day09.PartOne do
   def run([], _, record), do: record
 
-  def run([direction | tail], {0, 0, 0, 0}, record) do
-    case direction do
-      "R" -> run(tail, {0, 0, 1, 0}, record)
-      "L" -> run(tail, {0, 0, -1, 0}, record)
-      "U" -> run(tail, {0, 0, 0, 1}, record)
-      "D" -> run(tail, {0, 0, 0, -1}, record)
-    end
-  end
-
-  # 在同一行，y 轴相同
-  def run(["R" | tail], {t_x, y, h_x, y}, record) do
-    if h_x + 1 == t_x do
-      run(tail, {t_x, y, t_x, y}, record)
-    else
-      run(tail, {t_x + 1, y, h_x + 1, y}, MapSet.put(record, {t_x + 1, y}))
-    end
-  end
-
-  # H 在右上角
-  def run(["R" | tail], {t_x, t_y, h_x, h_y}, record)
-      when h_x - 1 == t_x and h_y - 1 == t_y do
-    run(tail, {t_x + 1, t_y + 1, h_x + 1, h_y}, MapSet.put(record, {t_x + 1, t_y + 1}))
-  end
-
-  # H 在左下角
-  def run(["R" | tail], {t_x, t_y, h_x, h_y}, record)
-      when h_x - 1 == t_x and h_y + 1 == t_y do
-    run(tail, {t_x + 1, t_y - 1, h_x + 1, h_y}, MapSet.put(record, {t_x + 1, t_y - 1}))
-  end
-
   def run(["R" | tail], {t_x, t_y, h_x, h_y}, record) do
-    run(tail, {t_x, t_y, h_x + 1, h_y}, record)
-  end
-
-  # 在同一列, x 轴相同
-  def run(["D" | tail], {x, t_y, x, h_y}, record) do
-    if h_y - 1 == t_y do
-      run(tail, {x, t_y, x, t_y}, record)
-    else
-      run(tail, {x, t_y + 1, x, h_y + 1}, MapSet.put(record, {x, t_y + 1}))
-    end
-  end
-
-  # H 在左下角
-  def run(["D" | tail], {t_x, t_y, h_x, h_y}, record)
-      when h_x - 1 == t_x and h_y + 1 == t_y do
-    run(tail, {t_x - 1, t_y - 1, h_x, h_y - 1}, MapSet.put(record, {t_x - 1, t_y - 1}))
-  end
-
-  # H 在右下角
-  def run(["D" | tail], {t_x, t_y, h_x, h_y}, record)
-      when h_x - 1 == t_x and h_y + 1 == t_y do
-    run(tail, {t_x + 1, t_y - 1, h_x, h_y - 1}, MapSet.put(record, {t_x + 1, t_y - 1}))
+    points = follow(t_x, t_y, h_x + 1, h_y)
+    run(tail, points, MapSet.put(record, {elem(points, 0), elem(points, 1)}))
   end
 
   def run(["D" | tail], {t_x, t_y, h_x, h_y}, record) do
-    run(tail, {t_x, t_y, h_x, h_y - 1}, record)
-  end
-
-  # 在同一行， y轴相同
-  def run(["L" | tail], {t_x, y, h_x, y}, record) do
-    if h_x - 1 == t_x do
-      run(tail, {t_x, y, t_x, y}, record)
-    else
-      run(tail, {h_x, y, h_x - 1, y}, MapSet.put(record, {h_x, y}))
-    end
-  end
-
-  # H 在左上角
-  def run(["L" | tail], {t_x, t_y, h_x, h_y}, record)
-      when h_x + 1 == t_x and h_y - 1 == t_y do
-    run(tail, {t_x - 1, t_y + 1, h_x - 1, h_y}, MapSet.put(record, {t_x - 1, t_y + 1}))
-  end
-
-  # H 在左下角
-  def run(["L" | tail], {t_x, t_y, h_x, h_y}, record)
-      when h_x - 1 == t_x and h_y + 1 == t_y do
-    run(tail, {t_x - 1, t_y - 1, h_x - 1, h_y}, MapSet.put(record, {t_x - 1, t_y - 1}))
+    points = follow(t_x, t_y, h_x, h_y - 1)
+    run(tail, points, MapSet.put(record, {elem(points, 0), elem(points, 1)}))
   end
 
   def run(["L" | tail], {t_x, t_y, h_x, h_y}, record) do
-    run(tail, {t_x, t_y, h_x - 1, h_y}, record)
-  end
-
-  # 在同一列，x 轴相同
-  def run(["U" | tail], {x, t_y, x, h_y}, record) do
-    if h_y + 1 == t_y do
-      run(tail, {x, t_y, x, t_y}, record)
-    else
-      run(tail, {x, t_y + 1, x, h_y + 1}, MapSet.put(record, {x, t_y + 1}))
-    end
-  end
-
-  # H 在左上角
-  def run(["U" | tail], {t_x, t_y, h_x, h_y}, record)
-      when h_x + 1 == t_x and h_y - 1 == t_y do
-    run(tail, {t_x - 1, t_y + 1, h_x, h_y + 1}, MapSet.put(record, {t_x - 1, t_y + 1}))
-  end
-
-  # H 在右上角
-  def run(["U" | tail], {t_x, t_y, h_x, h_y}, record)
-      when h_x - 1 == t_x and h_y - 1 == t_y do
-    run(tail, {t_x + 1, t_y + 1, h_x, h_y + 1}, MapSet.put(record, {t_x + 1, t_y + 1}))
+    points = follow(t_x, t_y, h_x - 1, h_y)
+    run(tail, points, MapSet.put(record, {elem(points, 0), elem(points, 1)}))
   end
 
   def run(["U" | tail], {t_x, t_y, h_x, h_y}, record) do
-    run(tail, {t_x, t_y, h_x, t_y + 1}, record)
+    points = follow(t_x, t_y, h_x, h_y + 1)
+    run(tail, points, MapSet.put(record, {elem(points, 0), elem(points, 1)}))
   end
+
+  def follow(t_x, t_y, h_x, h_y) when abs(h_x - t_x) <= 1 and 
+    abs(h_y - t_y) <= 1 do
+    {t_x, t_y, h_x, h_y}
+  end
+
+  def follow(t_x, t_y, h_x, h_y) do
+    {compute(h_x - t_x) + t_x, compute(h_y - t_y) + t_y, h_x, h_y} 
+  end
+  
+  defp compute(0), do: 0
+  defp compute(n), do: n |> abs() |> div(n) 
 end
 
 File.read!("./day_09/input.txt")
@@ -117,9 +40,7 @@ File.read!("./day_09/input.txt")
 |> Enum.flat_map(fn [action, step] ->
   List.duplicate(action, String.to_integer(step))
 end)
-|> IO.inspect()
 |> Day09.PartOne.run({0, 0, 0, 0}, MapSet.new([{0, 0}]))
-|> IO.inspect()
 |> MapSet.to_list()
 |> length()
 |> IO.inspect()

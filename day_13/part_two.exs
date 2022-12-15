25dc9980692d4d000ec32a130e28a36ed94c29ce
Taine Fan <fty221@gmail.com>
2022-12-15 15:52:18 +0800

    Finished Day13 Part Two

--- day_13/part_two.exs
+++ day_13/part_two.exs
@@ -1,24 +1,49 @@
 defmodule Day13.PartTwo do
   def run(packets) do
     (packets ++ [[[2]], [[6]]])
-    |> Enum.sort_by(&until_get_integer/1, :asc)
+    |> Enum.sort(fn a, b -> do_compare(a, b) in [true, :equal] end)
     |> Enum.with_index(1)
     |> Enum.filter(&(elem(&1, 0) in [[[2]], [[6]]]))
     |> Enum.map(&elem(&1, 1))
     |> Enum.product()
-    |> dbg()
   end
 
-  def until_get_integer([]), do: 0
-  def until_get_integer([[]]), do: 1
-  def until_get_integer([[[]]]), do: 2
+  def do_compare(left, right) when left == right, do: :equal
 
-  def until_get_integer([h | tail]) when is_integer(h) do
-    h
+  def do_compare([], _right), do: true
+
+  def do_compare(_left, []), do: false
+
+  def do_compare([l_h | l_tail], [r_h | r_tail])
+      when is_integer(l_h) and
+             is_integer(r_h) do
+    case l_h - r_h do
+      0 ->
+        do_compare(l_tail, r_tail)
+
+      result when result < 0 ->
+        true
+
+      _ ->
+        false
+    end
   end
 
-  def until_get_integer([h | tail]) do
-    until_get_integer(h)
+  def do_compare([l_h | l_tail], [r_h | r_tail])
+      when is_list(l_h) and is_list(r_h) do
+    case do_compare(l_h, r_h) do
+      :equal -> do_compare(l_tail, r_tail)
+      true -> true
+      _ -> false
+    end
+  end
+
+  def do_compare([l_h | _] = left, [r_h | r_tail]) when is_list(l_h) do
+    do_compare(left, [[r_h] | r_tail])
+  end
+
+  def do_compare([l_h | l_tail], [r_h | _] = right) when is_list(r_h) do
+    do_compare([[l_h] | l_tail], right)
   end
 end
 

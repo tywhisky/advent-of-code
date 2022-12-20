cc99aba40856afcf3395ff9cc3e88e6293be0bb2
tywhisky <fty221@gmail.com>
2022-12-20 16:07:18 +0800

    Finished Day 18

--- day_18/part_two.exs
+++ day_18/part_two.exs
@@ -1,18 +1,23 @@
 defmodule Day18.PartTwo do
-  def run([x, y, z], _, map)
+  def run([x, y, z], _, map, count)
       when x < 0 or y < 0 or z < 0 or
              x > 20 or y > 20 or z > 20,
-      do: [[x, y, z] | map]
+      do: {[[x, y, z] | map], count}
 
-  def run([x, y, z] = curr, total, map) do
-    if curr in total or curr in map do
-      [curr | map]
+  def run([x, y, z] = curr, total, map, count) do
+    if curr in total do
+      {[curr | map], count + 1}
     else
-      map = [curr | map]
-      [[x + 1, y, z], [x - 1, y, z], [x, y + 1, z], [x, y - 1, z], [x, y, z + 1], [x, y, z - 1]]
-      |> Enum.reduce(map, fn next, m ->
-          run(next, total, m)
-      end)
+      if curr in map do
+        {[curr | map], count}
+      else
+        map = [curr | map]
+
+        [[x + 1, y, z], [x - 1, y, z], [x, y + 1, z], [x, y - 1, z], [x, y, z + 1], [x, y, z - 1]]
+        |> Enum.reduce({map, count}, fn next, {m, c} ->
+          run(next, total, m, c)
+        end)
+      end
     end
   end
 end
@@ -23,7 +28,5 @@ input =
   |> Enum.map(&String.split(&1, ","))
   |> Enum.map(&Enum.map(&1, fn word -> String.to_integer(word) end))
 
-outside = Day18.PartTwo.run([0, 0, 0], input, [])
-
-input -- (input -- outside)
+Day18.PartTwo.run([0, 0, 0], input, [], 3)
 |> IO.inspect()

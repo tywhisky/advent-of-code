73dd75103c02b15a7ef4c76ea5c919803843f124
tywhisky <fty221@gmail.com>
2023-12-05 22:30:35 +0800

    PArtTwo

--- 2023/day_05/solution.exs
+++ 2023/day_05/solution.exs
@@ -7,6 +7,67 @@ defmodule Day05 do
     |> Enum.min()
   end
 
+  def part_two() do
+    [seeds | maps] = parse()
+
+    seeds
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [s, l] ->
+      find_range(maps, [{s, s + l - 1}])
+    end)
+    |> List.flatten()
+    |> Enum.reject(&(elem(&1, 0) == 0))
+    |> Enum.min_by(&elem(&1, 0))
+    |> elem(0)
+  end
+
+  defp find_range([], range), do: range
+
+  defp find_range([section | tail], range) do
+    new_range =
+      range
+      |> Enum.reduce([], fn {seed_start, seed_end}, acc ->
+        acc1 =
+          case Enum.find(section, fn {{s, e}, ds} ->
+                 seed_start >= s and seed_start <= e
+               end) do
+            nil ->
+              [{seed_start, seed_end} | acc]
+
+            {{ss, ee}, ds} when ee >= seed_end ->
+              [{ds - ss + seed_start, ds - ss + seed_end} | acc]
+
+            {{ss, ee}, ds} ->
+              [{ds - ss + seed_start, ee}, {ee + 1, seed_end} | acc]
+          end
+
+        acc2 =
+          case Enum.find(section, fn {{s, e}, ds} -> seed_end >= s and seed_end <= e end) do
+            nil ->
+              [{seed_start, seed_end} | acc1]
+
+            {{ss, ee}, ds} when ss <= seed_start ->
+              [{ds - ss + seed_start, ds - ss + seed_end} | acc1]
+
+            {{ss, ee}, ds} ->
+              [{seed_start, ss - 1}, {ds - ss + ss, ds - ss + seed_end} | acc1]
+          end
+
+        acc3 =
+          case Enum.find(section, fn {{s, e}, ds} -> seed_start < s and seed_end > e end) do
+            nil ->
+              acc2
+
+            {{ss, ee}, ds} ->
+              [{seed_start, ss - 1}, {ds - ss + ss, ds - ss + ee}, {ee + 1, seed_end} | acc2]
+          end
+
+        Enum.uniq(acc3)
+      end)
+
+    find_range(tail, new_range)
+  end
+
   defp find_location([], location), do: location
 
   defp find_location([section | tail], seed) do
@@ -50,3 +111,4 @@ defmodule Day05 do
 end
 
 IO.inspect("The result of Day05 part one is: #{Day05.part_one()}")
+IO.inspect("The result of Day05 part two is: #{Day05.part_two()}")

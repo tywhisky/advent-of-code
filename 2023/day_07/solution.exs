b830e7fc6c885118ba3c00d61284f89baf82f5ba
tywhisky <fty221@gmail.com>
2023-12-07 19:16:16 +0800

    P2

--- 2023/day_07/solution.exs
+++ 2023/day_07/solution.exs
@@ -1,10 +1,49 @@
 defmodule Day07 do
-  @alpha_order ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
-               |> Enum.with_index(1)
-               |> Map.new()
-
   def part_one() do
+    order =
+      ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+      |> Enum.with_index(1)
+      |> Map.new()
+
+    sort_and_sum(parse(), order)
+  end
+
+  def part_two() do
+    order =
+      ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+      |> Enum.with_index(1)
+      |> Map.new()
+
     parse()
+    |> Enum.map(fn {freq, list, bid} ->
+      f = Map.to_list(freq)
+
+      case freq["J"] do
+        nil ->
+          {freq, list, bid}
+
+        5 ->
+          {freq, list, bid}
+
+        qty ->
+          {max_chat, max_count} =
+            f
+            |> Enum.reject(&(elem(&1, 0) == "J"))
+            |> Enum.max_by(&elem(&1, 1))
+
+          new_freq =
+            freq
+            |> Map.delete("J")
+            |> Map.put(max_chat, qty + max_count)
+
+          {new_freq, list, bid}
+      end
+    end)
+    |> sort_and_sum(order)
+  end
+
+  def sort_and_sum(data, order) do
+    data
     |> Enum.sort(fn {freq1, list1, _bid1}, {freq2, list2, _bid2} ->
       f1 = Map.to_list(freq1)
       f2 = Map.to_list(freq2)
@@ -17,7 +56,7 @@ defmodule Day07 do
 
       if len1 == len2 do
         if pair1 == pair2 do
-          compare_first_char(list1, list2)
+          compare_first_char(list1, list2, order)
         else
           pair1 < pair2
         end
@@ -26,16 +65,18 @@ defmodule Day07 do
       end
     end)
     |> Enum.with_index(1)
-    |> Enum.map(fn {{_, _, bid}, idx} -> bid * idx  end)
+    |> Enum.map(fn {{_, _, bid}, idx} -> bid * idx end)
     |> Enum.sum()
   end
 
-  def compare_first_char([c1 | tail1], [c2 | tail2]) when c1 == c2 do
-    compare_first_char(tail1, tail2)
+  def compare_first_char([], [], _order), do: false
+
+  def compare_first_char([c1 | tail1], [c2 | tail2], order) when c1 == c2 do
+    compare_first_char(tail1, tail2, order)
   end
 
-  def compare_first_char([c1 | _], [c2 | _]) do
-    @alpha_order[c1] > @alpha_order[c2]
+  def compare_first_char([c1 | _], [c2 | _], order) do
+    order[c1] > order[c2]
   end
 
   def parse() do
@@ -52,3 +93,4 @@ defmodule Day07 do
 end
 
 IO.inspect("The result of Day07 part one is: #{Day07.part_one()}")
+IO.inspect("The result of Day07 part two is: #{Day07.part_two()}")

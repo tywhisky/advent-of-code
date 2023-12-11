263bc5c2ff8bf7a020e93ecefe4e495d71933d96
tywhisky <fty221@gmail.com>
2023-12-11 14:08:47 +0800

    opt

--- 2023/day_11/solution.exs
+++ 2023/day_11/solution.exs
@@ -1,37 +1,41 @@
 defmodule Day11 do
-  def part_one() do
+  def part_one(times \\ 2) do
     list = parse()
 
-    {empty_cols, empty_rows} = check_galaxies(list)
     galaxies = Enum.filter(list, &(elem(&1, 1) == "#")) |> Enum.map(&elem(&1, 0))
+    {empty_cols, empty_rows} = check_galaxies(list)
 
-    find_length(galaxies, [], empty_cols, empty_rows)
+    find_length(galaxies, [], empty_cols, empty_rows, times)
   end
 
-  def find_length([], result, _, _), do: Enum.sum(result)
+  def part_two() do
+    part_one(1_000_000)
+  end
 
-  def find_length([{hc, hr} | tail], result, cols, rows) do
+  def find_length([], result, _, _, _), do: Enum.sum(result)
+
+  def find_length([{hc, hr} | tail], result, cols, rows, times) do
     new_result =
       Enum.map(tail, fn {c, r} ->
         olength = abs(hc - c) + abs(hr - r)
 
         l1 = (cols -- Enum.to_list(hc..c)) |> length()
         l2 = (rows -- Enum.to_list(hr..r)) |> length()
-        length(cols) + length(rows) - l1 - l2 + olength
+        (length(cols) + length(rows) - l1 - l2) * (times - 1) + olength
       end)
 
-    find_length(tail, new_result ++ result, cols, rows)
+    find_length(tail, new_result ++ result, cols, rows, times)
   end
 
   def check_galaxies(list) do
     empty_cols =
-      Enum.group_by(list, fn {{col, row}, _} -> col end)
-      |> Enum.filter(fn {empty_col, col_list} -> Enum.all?(col_list, &(elem(&1, 1) == ".")) end)
+      Enum.group_by(list, fn {{col, _row}, _} -> col end)
+      |> Enum.filter(fn {_empty_col, col_list} -> Enum.all?(col_list, &(elem(&1, 1) == ".")) end)
       |> Enum.map(&elem(&1, 0))
 
     empty_rows =
-      Enum.group_by(list, fn {{col, row}, _} -> row end)
-      |> Enum.filter(fn {empty_col, col_list} -> Enum.all?(col_list, &(elem(&1, 1) == ".")) end)
+      Enum.group_by(list, fn {{_col, row}, _} -> row end)
+      |> Enum.filter(fn {_empty_col, col_list} -> Enum.all?(col_list, &(elem(&1, 1) == ".")) end)
       |> Enum.map(&elem(&1, 0))
 
     {empty_cols, empty_rows}
@@ -52,4 +56,4 @@ defmodule Day11 do
 end
 
 IO.puts("The Result of Day11 Part One is: #{Day11.part_one()}")
-# IO.puts("The Result of Day11 Part Two is: #{Day11.part_two()}")
+IO.puts("The Result of Day11 Part Two is: #{Day11.part_two()}")

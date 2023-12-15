3bf0f5bf79b99f40a619890f73d2a73e05478545
tywhisky <fty221@gmail.com>
2023-12-15 14:37:58 +0800

    Update

--- 2023/day_15/solution.exs
+++ 2023/day_15/solution.exs
@@ -8,11 +8,34 @@ defmodule Day15 do
       end)
     end)
     |> Enum.sum()
-    |> dbg()
+  end
+
+  def part_two() do
+    parse()
+    |> Enum.reduce([], fn char_list, acc ->
+      if ?= in char_list do
+        {_, list} = Enum.split_with(char_list, &(&1 == ?=))
+        [num | list] = Enum.reverse(list)
+        [{Enum.reverse(list), num - 48} | acc]
+      else
+        {_, list} = Enum.split_with(char_list, &(&1 == ?-))
+        tar = Enum.find(acc, &(elem(&1, 0) == list))
+        acc -- [tar]
+      end
+    end)
+    |> Enum.reverse()
+    |> Enum.reduce([], fn
+      {_char_list, num}, [] ->
+        [num * num]
+
+      {_char_list, num}, [h | _] = acc ->
+        [h * num | acc]
+    end)
+    |> Enum.sum()
   end
 
   def parse() do
-    "input.txt"
+    "test.txt"
     |> File.read!()
     |> String.replace("\n", "")
     |> String.split(",", trim: true)
@@ -21,4 +44,4 @@ defmodule Day15 do
 end
 
 IO.puts("The Result of Day15 Part One is: #{Day15.part_one()}")
-# IO.puts("The Result of Day15 Part Two is: #{Day15.part_two()}")
+IO.puts("The Result of Day15 Part Two is: #{Day15.part_two()}")

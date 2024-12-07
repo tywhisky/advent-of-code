f27f7199bb8f4e80ab66ee5b3911d632c8cc2103
tywhisky <fty221@gmail.com>
2024-12-07 15:55:08 +0800

    Update

--- 2024/day_05/solution.exs
+++ 2024/day_05/solution.exs
@@ -1,6 +1,6 @@
-defmodule Solution do
-  alias Solution.Sorter
+Mix.install([{:libgraph, "~> 0.16.0"}])
 
+defmodule Solution do
   def parse(path) do
     path
     |> File.read!()
@@ -35,32 +35,28 @@ defmodule Solution do
   def part_two(path) do
     {map, rows} = parse(path)
 
-    rows
-    |> Enum.reject(&check(&1, [], map))
-
     list = Map.to_list(map)
 
     all =
-      Enum.map(list, fn {k, v} -> [k | v] end)
-      |> List.flatten()
+      Enum.reduce(list, [], fn {k, v}, acc -> [k | v] ++ acc end)
       |> Enum.uniq()
-      |> IO.inspect()
 
-    Enum.reduce(list, all, fn {k, v}, acc ->
-      v
-      |> Enum.map(fn x -> [k, x] end)
-      |> Enum.reduce(acc, fn q, a ->
-        Sorter.sort_by_order(q, a)
-      end)
-      |> dbg()
+    order =
+      list
+      |> Enum.flat_map(fn {k, v} -> Enum.map(v, &{k, &1}) end)
+      |> case do
+        edges ->
+          Graph.new() |> Graph.add_vertices(all) |> Graph.add_edges(edges)
+      end
+      |> Graph.topsort()
+
+    Enum.reject(rows, &check(&1, [], map))
+    |> Enum.map(fn row ->
+      sort_by_order(row, order)
     end)
-  end
-
-  defmodule Sorter do
-    def sort_by_order(my_list, ordered_keys) do
-      index_map = Enum.with_index(ordered_keys) |> Enum.into(%{})
-      Enum.sort_by(my_list, fn key -> Map.get(index_map, key, -1) end)
-    end
+    |> Enum.map(&Enum.at(&1, div(length(&1), 2)))
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 
   def check([h | tail], [], map) do
@@ -76,10 +72,15 @@ defmodule Solution do
       false
     end
   end
+
+  def sort_by_order(my_list, ordered_keys) do
+    index_map = Enum.with_index(ordered_keys) |> Enum.into(%{})
+    Enum.sort_by(my_list, fn key -> Map.get(index_map, key, -1) end)
+  end
 end
 
 Solution.part_one("test.txt") |> IO.inspect(label: "Part One with test.txt")
 Solution.part_one("input.txt") |> IO.inspect(label: "Part One with input.txt")
 
 Solution.part_two("test.txt") |> IO.inspect(label: "Part Two with test.txt")
-# Solution.part_two("input.txt") |> IO.inspect(label: "Part Two with input.txt")
+Solution.part_two("input.txt") |> IO.inspect(label: "Part Two with input.txt")

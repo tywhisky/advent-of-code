2a1217ce83cfff1b314a4f71518bf92d1ddfa1e6
tywhisky <fty221@gmail.com>
2024-12-08 17:59:14 +0800

    Day07

--- 2024/day_07/solution.exs
+++ 2024/day_07/solution.exs
@@ -33,10 +33,34 @@ defmodule Solution do
   def do_check([h | tail], sum, curr) do
     do_check(tail, sum, curr * h) || do_check(tail, sum, curr + h)
   end
+
+  def part_two(path) do
+    path
+    |> parse()
+    |> Enum.filter(fn {sum, nums} ->
+      check2(nums, sum)
+    end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
+  end
+
+  def check2([h | tail], sum) do
+    do_check2(tail, sum, h)
+  end
+
+  def do_check2([], sum, curr), do: sum == curr
+
+  def do_check2([h | tail], sum, curr) do
+    connect = "#{curr}#{h}" |> String.to_integer()
+
+    do_check2(tail, sum, curr * h) ||
+      do_check2(tail, sum, curr + h) ||
+      do_check2(tail, sum, connect)
+  end
 end
 
 Solution.part_one("test.txt") |> IO.inspect(label: "Part One with test.txt")
 Solution.part_one("input.txt") |> IO.inspect(label: "Part One with input.txt")
 
-# Solution.part_two("test.txt") |> IO.inspect(label: "Part Two with test.txt")
-# Solution.part_two("input.txt") |> IO.inspect(label: "Part Two with input.txt")
+Solution.part_two("test.txt") |> IO.inspect(label: "Part Two with test.txt")
+Solution.part_two("input.txt") |> IO.inspect(label: "Part Two with inpt.txt")

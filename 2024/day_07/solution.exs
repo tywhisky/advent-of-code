839bb869e106c3865903179a96277d80be001c7c
tywhisky <fty221@gmail.com>
2024-12-08 17:22:25 +0800

    Update

---
+++ 2024/day_07/solution.exs
@@ -0,0 +1,42 @@
+defmodule Solution do
+  def parse(path) do
+    path
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, ":", trim: true))
+    |> Enum.map(fn [h, str] ->
+      nums =
+        str
+        |> String.split(" ", trim: true)
+        |> Enum.map(&String.to_integer/1)
+
+      {String.to_integer(h), nums}
+    end)
+  end
+
+  def part_one(path) do
+    path
+    |> parse()
+    |> Enum.filter(fn {sum, nums} ->
+      check(nums, sum)
+    end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
+  end
+
+  def check([h | tail], sum) do
+    do_check(tail, sum, h)
+  end
+
+  def do_check([], sum, curr), do: sum == curr
+
+  def do_check([h | tail], sum, curr) do
+    do_check(tail, sum, curr * h) || do_check(tail, sum, curr + h)
+  end
+end
+
+Solution.part_one("test.txt") |> IO.inspect(label: "Part One with test.txt")
+Solution.part_one("input.txt") |> IO.inspect(label: "Part One with input.txt")
+
+# Solution.part_two("test.txt") |> IO.inspect(label: "Part Two with test.txt")
+# Solution.part_two("input.txt") |> IO.inspect(label: "Part Two with input.txt")

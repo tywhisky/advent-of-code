d39141285fdf8f76f5a63ea8e5f66740121578b4
tywhisky <fty221@gmail.com>
2024-12-14 22:09:25 +0800

    Update day 14 part 2

--- 2024/day_14/solution.exs
+++ 2024/day_14/solution.exs
@@ -1,3 +1,42 @@
+Mix.install([
+  {:egd, github: "erlang/egd"}
+])
+
+defmodule DotMatrixImage do
+  def generate_image(matrix, output_path \\ "output.png") do
+    cell_size = 10
+    width = String.length(Enum.at(matrix, 0)) * cell_size
+    height = length(matrix) * cell_size
+
+    image = :egd.create(width, height)
+    fill_color = :egd.color(:green)
+    background_color = :egd.color(:black)
+
+    :egd.filledRectangle(image, {0, 0}, {width - 1, height - 1}, background_color)
+
+    Enum.with_index(matrix)
+    |> Enum.each(fn {row, row_idx} ->
+      String.graphemes(row)
+      |> Enum.with_index()
+      |> Enum.each(fn
+        {"#", col_idx} ->
+          x1 = col_idx * cell_size
+          y1 = row_idx * cell_size
+          x2 = x1 + cell_size - 1
+          y2 = y1 + cell_size - 1
+          :egd.filledRectangle(image, {x1, y1}, {x2, y2}, fill_color)
+
+        _ ->
+          :ok
+      end)
+    end)
+
+    a = :egd.render(image)
+
+    File.write(output_path, a)
+  end
+end
+
 defmodule Solution do
   def parse(path) do
     path
@@ -28,111 +67,41 @@ defmodule Solution do
   end
 
   def part_two(path, wide, tall) do
-    path
-    |> parse()
-    |> Stream.iterate(fn point ->
-      one_second(point, wide, tall, [])
+    list_v = 33..10000 |> Enum.take_every(wide)
+    list_h = 87..10000 |> Enum.take_every(tall)
+
+    stream =
+      path
+      |> parse()
+      |> Stream.iterate(fn point ->
+        one_second(point, wide, tall, [])
+      end)
+
+    (list_v ++ list_h)
+    |> Enum.each(fn idx ->
+      matrix =
+        stream
+        |> Enum.at(idx)
+        |> display(wide, tall)
+
+      file_name = "#{idx}.png"
+      DotMatrixImage.generate_image(matrix, file_name)
     end)
-    |> Enum.at(100)
-    |> display(wide, tall)
+
+    :ok
   end
 
   def display(points, wide, tall) do
-    v_mid = div(wide, 2)
-    h_mid = div(tall, 2)
-
-    group_1 =
-      Enum.filter(points, fn [x, y, _, _] -> x < v_mid and y < h_mid end)
-      |> Enum.map(fn [x, y, _, _] -> {{x, y}, "#"} end)
-      |> Map.new()
-
-    group_2 =
-      Enum.filter(points, fn [x, y, _, _] -> x > v_mid and y < h_mid end)
-      |> Enum.map(fn [x, y, _, _] -> {{x, y}, "#"} end)
-      |> Map.new()
-
-    group_3 =
-      Enum.filter(points, fn [x, y, _, _] -> x < v_mid and y > h_mid end)
-      |> Enum.map(fn [x, y, _, _] -> {{x, y}, "#"} end)
-      |> Map.new()
-
-    group_4 =
-      Enum.filter(points, fn [x, y, _, _] -> x > v_mid and y > h_mid end)
-      |> Enum.map(fn [x, y, _, _] -> {{x, y}, "#"} end)
-      |> Map.new()
-
-    map_1 =
-      for i <- 0..(v_mid - 1), j <- 0..(h_mid - 1) do
+    map =
+      for i <- 0..(wide - 1), j <- 0..(tall - 1) do
         {{i, j}, "."}
       end
       |> Map.new()
-      |> Map.merge(group_1)
-      |> Enum.group_by(fn {a, _b} -> a end)
-      |> Enum.group_by(fn {{a, _}, _} -> a end)
-      |> Map.to_list()
-      |> Enum.sort_by(fn {k, _} -> k end)
-      |> Enum.map(fn {_, v} ->
-        v
-        |> Enum.sort_by(fn {{_, k}, _} -> k end)
-        |> Enum.map(&elem(&1, 1))
-        |> Enum.join("")
-      end)
-      |> IO.inspect()
 
-    map_2 =
-      for i <- (v_mid + 1)..(wide - 1), j <- 0..(h_mid - 1) do
-        {{i, j}, "."}
-      end
-      |> Map.new()
-      |> Map.merge(group_2)
-      |> Enum.group_by(fn {{a, _}, _} -> a end)
-      |> Map.to_list()
-      |> Enum.sort_by(fn {k, _} -> k end)
-      |> Enum.map(fn {_, v} ->
-        v
-        |> Enum.sort_by(fn {{_, k}, _} -> k end)
-        |> Enum.map(&elem(&1, 1))
-        |> Enum.join("")
-      end)
-      |> IO.inspect()
+    p_map = Map.new(points, fn [a, b, _, _] -> {{a, b}, "#"} end)
 
-    map_3 =
-      for i <- 0..(v_mid - 1), j <- (h_mid + 1)..(tall - 1) do
-        {{i, j}, "."}
-      end
-      |> Map.new()
-      |> Map.merge(group_3)
-      |> Enum.group_by(fn {{a, _}, _} -> a end)
-      |> Map.to_list()
-      |> Enum.sort_by(fn {k, _} -> k end)
-      |> Enum.map(fn {_, v} ->
-        v
-        |> Enum.sort_by(fn {{_, k}, _} -> k end)
-        |> Enum.map(&elem(&1, 1))
-        |> Enum.join("")
-      end)
-      |> IO.inspect()
-
-    map_4 =
-      for i <- (v_mid + 1)..(wide - 1), j <- (h_mid + 1)..(tall - 1) do
-        {{i, j}, "."}
-      end
-      |> Map.new()
-      |> Map.merge(group_4)
-      |> Enum.group_by(fn {{a, _}, _} -> a end)
-      |> Map.to_list()
-      |> Enum.sort_by(fn {k, _} -> k end)
-      |> Enum.map(fn {_, v} ->
-        v
-        |> Enum.sort_by(fn {{_, k}, _} -> k end)
-        |> Enum.map(&elem(&1, 1))
-        |> Enum.join("")
-      end)
-      |> IO.inspect()
-  end
-
-  def do_display(map) do
     map
+    |> Map.merge(p_map)
     |> Enum.group_by(fn {{a, _}, _} -> a end)
     |> Map.to_list()
     |> Enum.sort_by(fn {k, _} -> k end)
@@ -172,7 +141,8 @@ defmodule Solution do
   end
 end
 
-Solution.part_one("test.txt", 11, 7) |> IO.inspect(label: "Part One with test.txt")
-Solution.part_one("input.txt", 101, 103) |> IO.inspect(label: "Part One with input.txt")
+# Solution.part_one("test.txt", 11, 7) |> IO.inspect(label: "Part One with test.txt")
+# Solution.part_one("input.txt", 101, 103) |> IO.inspect(label: "Part One with input.txt")
 # Solution.part_two("test.txt") |> IO.inspect(label: "Part Two with test.txt")
-Solution.part_two("input.txt", 101, 103) |> IO.inspect(label: "Part Two with inpt.txt")
+Solution.part_two("input.txt", 101, 103)
+|> IO.inspect(label: "Part Two with inpt.txt")

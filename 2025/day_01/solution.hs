58d2aebd0551c17381e37ead00cb073ec1ce4764
Taine Fan <fty221@gmail.com>
2025-12-02 11:04:19 +0800

    Done Part 2 of Day 01 challenge for 2025

--- 2025/day_01/solution.hs
+++ 2025/day_01/solution.hs
@@ -1,11 +1,27 @@
-import Data.List (uncons)
-import Data.Maybe (fromJust)
+import System.Environment (getArgs)
 
-parseToLines :: IO [String]
-parseToLines = do
-  contents <- readFile "input.txt"
-  let ls = lines contents
-  return ls
+parseToLines :: FilePath -> IO [String]
+parseToLines fp = do
+  contents <- readFile fp
+  return (lines contents)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let fp = case args of
+        (x:_) -> x
+        _ -> "input.txt"
+
+  putStrLn $ "Using file: " ++ fp
+  lines <- parseToLines fp
+
+  putStrLn "\n=== Part One ==="
+  let (_, result1) = partOne lines [50]
+  print result1
+
+  putStrLn "\n=== Part Two ==="
+  let (_, result2) = partTwo lines [(50, 0)]
+  print result2
 
 partOne :: [String] -> [Int] -> ([Int], Int)
 partOne [] results = (results, length (filter (== 0) results))
@@ -25,3 +41,38 @@ partOne (line:rest) results =
     newResults = mod acc 100 : results
   in
     partOne rest newResults
+
+partTwo :: [String] -> [(Int, Int)] -> ([(Int, Int)], Int)
+partTwo [] results =
+  (results, sum (map snd results))
+
+partTwo (line:rest) results =
+  case line of
+    (d:nStr) ->
+      let
+        n    = read nStr :: Int
+        curr = case results of
+                 []     -> 50
+                 (x:_)  -> fst x
+
+        count =
+          case d of
+            'R' -> (curr + n) `div` 100
+            'L' -> if curr == 0
+                     then n `div` 100
+                     else if n < curr
+                       then 0
+                       else 1 + (n - curr) `div` 100
+            _   -> 0
+
+        delta = case d of
+                  'R' -> n
+                  'L' -> -n
+                  _   -> 0
+        newPos = (curr + delta) `mod` 100
+
+        newResults = (newPos, count) : results
+      in
+        partTwo rest newResults
+
+    _ -> error ("Bad input line: " ++ show line)

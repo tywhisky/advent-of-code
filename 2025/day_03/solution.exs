da3c5e9a6d924b91b366122807520b4e4f019793
Taine Fan <fty221@gmail.com>
2025-12-03 17:54:59 +0800

    Done day 03 solution

--- 2025/day_03/solution.exs
+++ 2025/day_03/solution.exs
@@ -1,13 +1,14 @@
 defmodule Solution do
+  @k 12
   def parse(input) do
     input
     |> File.read!()
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.graphemes/1)
   end
 
   def part_one(input) do
     parse(input)
+    |> Enum.map(&String.graphemes/1)
     |> Enum.map(fn line ->
       [{fst, fst_idx}, {snd, snd_idx} | rest] =
         line
@@ -31,6 +32,55 @@ defmodule Solution do
     |> Enum.sum()
     |> IO.inspect(label: "Part One Result")
   end
+
+  def part_two(input) do
+    parse(input)
+    |> Enum.map(&pick_max_12/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+    |> dbg()
+  end
+
+  def pick_max_12(digits) do
+    digits = String.graphemes(digits)
+    n = length(digits)
+    cache = :ets.new(:dp_cache, [:set, :public])
+
+    res =
+      dp(0, @k, digits, n, cache)
+
+    :ets.delete(cache)
+    res
+  end
+
+  defp dp(_i, 0, _digits, _n, _cache), do: ""
+  defp dp(i, j, _digits, n, _cache) when n - i < j, do: nil
+
+  defp dp(i, j, digits, n, cache) do
+    case :ets.lookup(cache, {i, j}) do
+      [{_, v}] ->
+        v
+
+      [] ->
+        max_pos = n - j
+
+        {best_digit, best_index} =
+          i..max_pos
+          |> Enum.map(&{Enum.at(digits, &1), &1})
+          |> Enum.max_by(fn {d, idx} -> {d, -idx} end)
+
+        tail = dp(best_index + 1, j - 1, digits, n, cache)
+
+        res =
+          case tail do
+            nil -> nil
+            _ -> best_digit <> tail
+          end
+
+        :ets.insert(cache, {{i, j}, res})
+        res
+    end
+end
 end
 
 file_path =
@@ -39,4 +89,5 @@ file_path =
     _ -> "input.txt"
   end
 
-Solution.part_one(file_path)
+# Solution.part_one(file_path)
+Solution.part_two(file_path)
